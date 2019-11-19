5fc27a65293c88f5aca2b32d42f7352957d2ff77
François Pottier <francois.pottier@inria.fr>
2019-11-19 18:56:59 +0100

    Implement [IFSeq.sample].

--- src/IFSeq.ml
+++ src/IFSeq.ml
@@ -3,5 +3,39 @@
 
 include IFSeqSyn.Make(Z)
 
+(* Iterated sum. *)
+
 let bigsum ss =
   List.fold_left sum zero ss
+
+(* For some reason, [Z.random] does not exist. *)
+(* For some reason, [Random.int] stops working at [2^30]. *)
+(* This is troublesome. *)
+
+let random_bigint (n : Z.t) : Z.t =
+  let open Z in
+  if n < one lsl 30 then
+    Z.of_int (Random.int (Z.to_int n))
+  else
+    failwith "Can't sample over more than 2^30 elements."
+      (* TEMPORARY really unsatisfactory! *)
+
+(* Extract a randomly chosen sample of [m] elements out of a sequence [s]. *)
+
+(* We do not protect against repetitions, as they are unlikely when [s] is
+   long. *)
+
+let rec sample (m : int) (s : 'a seq) : 'a Seq.t =
+  if m > 0 then
+    fun () ->
+      let i = random_bigint (length s) in
+      let x = get s i in
+      Seq.Cons (x, sample (m - 1) s)
+  else
+    Seq.empty
+
+(* If the sequence [s] is short enough, then produce all of its elements;
+   otherwise produce a randomly chosen sample, as above. *)
+
+let sample (m : int) (s : 'a seq) : 'a Seq.t =
+  if length s <= Z.of_int m then to_seq s else sample m s
--- src/IFSeq.mli
+++ src/IFSeq.mli
@@ -7,3 +7,9 @@ include IFSEQ with type index = Z.t
 
 (* Iterated sum. *)
 val bigsum: 'a seq list -> 'a seq
+
+(* [sample m s] is an explicit sequence of at most [m] elements extracted
+   out of the implicit sequence [s]. If [length s] at most [m], then all
+   elements of [s] are produced. Otherwise, a random sample of [m] elements
+   is extracted out of [s]. *)
+val sample: int -> 'a seq -> 'a Seq.t
